f40e232c1e0b2f7d9a69faa9b1490d4d53e0fb0a
Mohammad A Immam <immammohammad1@gmail.com>
2024-01-27 12:14:50 -0500

    Update spa.js

--- src/middlewares/spa.js
+++ src/middlewares/spa.js
@@ -8,17 +8,10 @@ module.exports = (config, { strapi }) => {
   // Add your own logic here.
   return async (ctx, next) => {
     strapi.log.info('In spa middleware.');
-    const { request: { path }, res, response } = ctx
-
-    if (ctx.path === "/") {
-      await sendfile(ctx, __dirname + '/../../public/index.html')
-    }
-
     await next();
-
-    if (response.status === 404) {
+    const { request: { path }, res, response } = ctx
+    if (path === "index.html" || response.status === 404) {
       await sendfile(ctx, __dirname + '/../../public/index.html')
     }
-
   };
 };
